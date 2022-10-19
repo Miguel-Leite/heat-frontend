d99104a5e1f9d24d3ac7d02f237f9ac05e0b3d6a
Miguel-Leite <miguelleite200leite@email.com>
2022-10-19 09:51:38 -0700

    component messagelist created

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,49 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList () {
     return (
-        <h1>Message List</h1>
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="Dowhile 2022" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/Miguel-Leite.png" alt="Miguel Leite" />
+                        </div>
+                        <span>Miguel Leite</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/Miguel-Leite.png" alt="Miguel Leite" />
+                        </div>
+                        <span>Miguel Leite</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/Miguel-Leite.png" alt="Miguel Leite" />
+                        </div>
+                        <span>Miguel Leite</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
