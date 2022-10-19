003d7cb026cfd4f0845f7828cc39953ddb8cb4c8
Miguel-Leite <miguelleite200leite@email.com>
2022-10-19 13:07:33 -0700

    component LoginBox done

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,17 +1,19 @@
+import { useContext } from 'react';
 import { VscGithub } from 'react-icons/vsc';
+import { AuthContext } from '../../contexts/auth';
 
 import styles from './styles.module.scss';
 
-export function LoginBox () {
+export function LoginBox() {
+  const { signInUrl } = useContext(AuthContext);
 
   return (
     <div className={styles.loginBoxWrapper}>
-        <strong>Entre e compartilhe a sua mensagem</strong>
-        <a href="#" className={styles.signInWithGithub}> 
-          <VscGithub size={24} /> Entrar com Github 
-        </a>
+      <strong>Entre e compartilhe a sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithub size={24} /> Entrar com Github
+      </a>
     </div>
   )
 }
 
-  
