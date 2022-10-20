8416c37fb25c7319e5b1c78d94c72d7e25d09c72
Miguel-Leite <miguelleite200leite@email.com>
2022-10-20 22:40:15 +0100

    list messages with socket.io-client

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,12 +1,36 @@
 import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+
 import styles from './styles.module.scss';
 
 import logoImg from '../../assets/logo.svg';
 import { api } from '../../services/api';
 import { TMessage } from '../../types/TMessage';
 
+const messageQueue: TMessage[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: TMessage) => {
+  messageQueue.push(newMessage)
+})
+
 export function MessageList () {
     const [messages, setMessages] = useState<TMessage[]>([])
+
+    useEffect(()=>{
+      const timer = setInterval(()=>{
+        if (messageQueue.length > 0) {
+          setMessages(prevState =>[
+            messageQueue[0],
+            prevState[0],
+            prevState[1], 
+          ].filter(Boolean))
+
+          messageQueue.shift();
+        }
+      },3000);
+    },[])
 
     useEffect(()=>{
         api.get<TMessage[]>('messages/last3').then(response => {
