b653ee2aa88522a8bad8715318f08bd39b88a160
Miguel-Leite <miguelleite200leite@email.com>
2022-10-20 21:40:39 +0100

    component message finish

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,5 +1,55 @@
+import { FormEvent, useContext, useState } from 'react';
+import { VscSignOut, VscGithub } from 'react-icons/vsc';
+import { AuthContext } from '../../contexts/auth';
+import { api } from '../../services/api';
+import styles from "./styles.module.scss";
+
 export function SendMessageForm () {
-    return (
-        <h1>Send Message Form</h1>
-    )
+  const { user, signOut } = useContext(AuthContext);
+  const [message, setMessage] = useState('');
+
+  async function handleSendMessage(e: FormEvent) {
+    e.preventDefault()
+
+    if (!message.trim()){
+      return;
+    }
+    
+    await api.post('messages', { message });
+
+    setMessage('');
+
+  }
+
+  return (
+    <div className={styles.sendMessageFormWrapper}>
+      <button className={styles.signOutButton} onClick={signOut}>
+        <VscSignOut size={32} />
+      </button>
+
+      <header className={styles.userInformation}>
+        <div className={styles.userImage}>
+          <img src={user?.avatar_url} alt={user?.name} />
+        </div>
+        <strong className={styles.userName}>{user?.name}</strong>
+        <span className={styles.userGithub}> 
+          <VscGithub size={16} /> 
+          {user?.login} 
+        </span>
+      </header>
+
+      <form onSubmit={handleSendMessage} className={styles.sendMessageForm}>
+        <label htmlFor="message">Mensagem</label>
+        <textarea 
+        name="message" 
+        id="message" 
+        placeholder='Qual é a sua expectativa para o evento?'
+        onChange={(e)=> setMessage(e.target.value)}
+        value={message}
+        required
+        />
+        <button>Enviar Mensagem</button>
+      </form>
+    </div>
+  )
 }
